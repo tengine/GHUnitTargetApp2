8588ce82787f3d7703c19eb44f7b51cb30877193
akima <akm2000@gmail.com>
2013-04-04 14:26:10 +0900

    Add Empty Application 'Tests' for GHUnit

---
+++ Tests/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tests
+//
+//  Created by  on 13/04/04.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
